6aecc153a600ab9dd661eb311e07a9e1ac8a65b8
webonise <webonise@webonises-mac-mini-6.local>
2016-09-22 11:42:00 +0530

    Initial Commit

---
+++ MVPSampleApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVPSampleApp
+//
+//  Created by webonise on 22/09/16.
+//  Copyright © 2016 webonise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVPSampleApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MVPSampleApp
+//
+//  Created by webonise on 22/09/16.
+//  Copyright © 2016 webonise. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
